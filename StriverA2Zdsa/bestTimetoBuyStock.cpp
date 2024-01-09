a97679999c81a436240d0e7880dd1f17d216ec1a
Rahul Verma <98616820+Rahulverma5@users.noreply.github.com>
2024-01-09 20:47:16 +0530

    Create bestTimetoBuyStock.cpp

---
+++ StriverA2Zdsa/bestTimetoBuyStock.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+//bruteforce solution is to find all possible profits and then take maximum one: O(n^2)
+
+//optimal solution
+int maxProfit(vector<int> &arr) {
+    int maxPro = 0;
+    int n = arr.size();
+    int minPrice = INT_MAX;
+
+    for (int i = 0; i < arr.size(); i++) {
+        minPrice = min(minPrice, arr[i]);
+        maxPro = max(maxPro, arr[i] - minPrice);
+    }
+    
+    return maxPro;
+}
