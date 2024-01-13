68147a8caf1003a2cada211697cba19a479d1cab
Rahul Verma <98616820+Rahulverma5@users.noreply.github.com>
2024-01-13 13:01:20 +0530

    Create longestCommPrefix.cpp

---
+++ StriverA2Zdsa/longestCommPrefix.cpp
@@ -0,0 +1,22 @@
+string longestCommonPrefix(vector<string>& strs) {
+    int n = strs.size();
+
+    string ans = strs[0];
+
+    if(n==1){
+        return strs[0];
+    }
+
+    for(int i=0; i<n-1;i++){
+        int j = 0;
+        string w="";
+        while(j<min(strs[i].length(), strs[i+1].length()) && strs[i][j]==strs[i+1][j] ){
+            w = w+strs[i][j];
+            j++;
+        }
+        if(ans.length()>w.length()){
+            ans = w;
+        }
+    }
+    return ans;   
+}
