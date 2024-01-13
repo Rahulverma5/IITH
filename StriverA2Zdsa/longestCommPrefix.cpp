4131a178c5b313f647b5070a6c9960a276e75976
Rahul Verma <98616820+Rahulverma5@users.noreply.github.com>
2024-01-13 13:10:25 +0530

    Update longestCommPrefix.cpp

--- StriverA2Zdsa/longestCommPrefix.cpp
+++ StriverA2Zdsa/longestCommPrefix.cpp
@@ -19,3 +19,18 @@ string longestCommonPrefix(vector<string>& strs) {
     }
     return ans;   
 }
+
+//better solution
+string longestCommonPrefix(vector<string>& v) {
+    string ans="";
+    sort(v.begin(),v.end());
+    int n=v.size();
+    string first=v[0],last=v[n-1];
+    for(int i=0;i<min(first.size(),last.size());i++){
+        if(first[i]!=last[i]){
+            return ans;
+        }
+        ans+=first[i];
+    }
+    return ans;
+}
