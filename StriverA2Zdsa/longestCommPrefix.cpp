8bed926d1856ba6c80a7dbb67e388f950b7ea7c5
Rahul Verma <98616820+Rahulverma5@users.noreply.github.com>
2024-01-13 13:02:51 +0530

    Update longestCommPrefix.cpp

--- StriverA2Zdsa/longestCommPrefix.cpp
+++ StriverA2Zdsa/longestCommPrefix.cpp
@@ -1,12 +1,11 @@
+
+//time complexity: O(m*n) where m is maximum common prefix and n is length of array
+
 string longestCommonPrefix(vector<string>& strs) {
     int n = strs.size();
-
     string ans = strs[0];
-
-    if(n==1){
-        return strs[0];
-    }
-
+    if(n==1) return strs[0];
+    
     for(int i=0; i<n-1;i++){
         int j = 0;
         string w="";
