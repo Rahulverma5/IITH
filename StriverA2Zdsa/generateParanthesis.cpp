74a00f6efd28ad5fa1582094b32887b6c932b405
Rahul Verma <98616820+Rahulverma5@users.noreply.github.com>
2024-01-23 17:14:16 +0530

    Create generateParanthesis.cpp

---
+++ StriverA2Zdsa/generateParanthesis.cpp
@@ -0,0 +1,30 @@
+
+//recursion+backtracking
+void generate (vector<string> &ans, string &s, int open, int close){
+      if(open==0 && close==0){
+          ans.push_back(s);
+          return;
+      }
+
+      if(open>0){
+          s.push_back('(');
+          generate(ans, s, open-1, close);
+          s.pop_back();
+      }
+
+      if(close>0){
+          if(open<close){
+               s.push_back(')');
+              generate(ans, s, open, close-1);
+              s.pop_back();
+          }      
+      }
+
+  }
+
+  vector<string> generateParenthesis(int n) {
+      vector<string> ans;
+      string s="";
+      generate(ans, s, n, n);
+      return ans;    
+  }
